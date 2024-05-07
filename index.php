57db4cc7f94d28e4f9b295748b3ff3174574b81d
ruwie <ruwiecortez@gmail.com>
2024-05-07 10:37:32 +0800

    role interface done

--- index.php
+++ index.php
@@ -21,9 +21,11 @@
         <div class="col-md-4" id="home-page">
             <center><h3 class="text-white">Choose login role</h3></center>
             <br>
-            <a href="" class="btn btn-warning">User login</a>
-            <a href="" class="btn btn-success">User registration</a>
+            <a href="" class="btn btn-warning " style="margin-right: 20px;">User login</a>
+            <a href="" class="btn btn-success" style="margin-right: 20px;">User registration</a>
             <a href="" class="btn btn-primary">Admin login</a>
+            <br>
+            <img src="media/usericon.png" alt="usericon" style="padding-top: 20px;">
         </div>
     </div>
 </body>
