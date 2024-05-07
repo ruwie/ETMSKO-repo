5e8be87648ed4623f52584c87f354daae1cace71
ruwie <ruwiecortez@gmail.com>
2024-05-07 09:06:37 +0800

    bootstrap is working

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ETMSKO</title>
+
+    <!-- JQUERY file -->
+    <script src="includes/jquery-3.7.1.min.js"></script>
+
+    <!-- Bootstrap file -->
+    <script src="bootstrap/bootstrap.min.css"></script>
+    <script src="bootstrap/bootstrap.min.js"></script>
+</head>
+<body>
+    Hello world!
+</body>
+</html>
